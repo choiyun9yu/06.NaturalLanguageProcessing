a4280d41fa11cf609893232d27de1b3e3ea6f111
choiyun9yu <sw22tapple@gmail.com>
2023-03-22 21:12:14 +0900

    main

--- first/src/JavaStudy/HelloWorld.java
+++ first/src/JavaStudy/HelloWorld.java
@@ -1,5 +1,17 @@
-package JavaStudy;
+package first;
 
 public class HelloWorld {
-
+//	프로그램의 시작점.
+	/** 문서화 주석 
+	 * 
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+//		System.out.println("test");
+/*		System.out.println("1");
+		System.out.println("2");
+		System.out.println("3");	*/
+		System.out.println("주석 단축키 Ctrl+/");
+	}
 }
