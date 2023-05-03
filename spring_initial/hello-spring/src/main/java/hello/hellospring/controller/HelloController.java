7a834f526c9143eb1d66b9213393b522df2eb4fd
choiyun9yu <sw22tapple@gmail.com>
2023-05-03 18:20:00 +0900

    main

--- spring_initial/hello-spring/src/main/java/hello/hellospring/controller/HelloController.java
+++ spring_initial/hello-spring/src/main/java/hello/hellospring/controller/HelloController.java
@@ -1,5 +1,6 @@
 package hello.hellospring.controller;
 
+import hello.hellospring.HelloSpringApplication;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -25,5 +26,25 @@ public class HelloController {
         @ResponseBody
         public String helloString(@RequestParam("name") String name) {
             return "hello " + name;
+        }
+
+        @GetMapping("hello-api")
+        @ResponseBody
+        public Hello helloApi(@RequestParam("name") String name){
+            Hello hello = new Hello();
+            hello.setName(name);
+            return hello;
+        }
+
+        static class Hello{
+            private String name;
+
+
+            public String getName(){
+                return name;
+            }
+            public void setName(String name){
+                this.name=name;
+            }
         }
 }
