05fc89ea15c1d7597a40e31490d4bafca6794074
choiyun9yu <sw22tapple@gmail.com>
2023-05-09 00:09:44 +0900

    main

--- 김영한의 Spring/hello-spring/src/main/java/hello/hellospring/domain/Member.java
+++ 김영한의 Spring/hello-spring/src/main/java/hello/hellospring/domain/Member.java
@@ -1,8 +1,13 @@
 package hello.hellospring.domain;
 
+import jakarta.persistence.*;
+
+@Entity
 public class Member {
 
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;    // 데이터를 구분하기 위해 시스템에 저장되는 id
+
     private String name;
 
     public Long getId() {
