f0f42d9a0795d4ba879a5d0deea5187078f2c179
choiyun9yu <sw22tapple@gmail.com>
2023-05-09 22:20:42 +0900

    main

---
+++ ThisIsJava/Part1/IntegerLiteralExample.java
@@ -0,0 +1,18 @@
+package Part1;
+
+public class IntegerLiteralExample {
+    public static void main(String[] args) {
+        int var2 = 0b1101;  // 2진법
+        int var8 = 015;     // 8진법
+        int var10 = 13;     // 10진법
+        int var16 = 0xD;    // 16진컴파일러는 int로 간주하기 때문에 에러 발생 라이릳갸아다갸ㅓㅓ
+//      System.out.println(var2, var8, var10, var16); 자바는 파이썬 처럼 ,로 프린트 할 수 없음
+        System.out.println(var2 + "," + var8 + "," + var10 + "," + var16);
+
+//      long var1 = 1000000000000;    컴파일러는 in로 간주하기 때문에 l을 붙여서 long type 임을 컴파일러에게 알려줘야 한다.
+        long var11 = 1000000000000L;
+
+        float f1 = 3.14;   //  컴파일러가 double로 인식
+        float f2 = 3.14f;  // 컴파일러가 fㅣoat으로 인식
+    }
+}
---
+++ ThisIsJava/Part1/StringExample.java
@@ -0,0 +1,47 @@
+package Part1;
+
+public class StringExample {
+    public static void main(String[] args) {
+//      char var1 = "A";    큰따옴표로 감싸면 char 컴파일 에러
+//      char var2 = 'abc';
+        char var1 = 'A';
+
+        String name = "홍길동";
+        String job = "프로그래머";
+        System.out.println(name);
+        System.out.println(job);
+
+        String str = "나는 \"자바\"를 배웁니다.";
+        System.out.println(str);
+
+        str = "번호\t이\t직업";
+        System.out.println(str);
+
+        System.out.print("나는\n");
+        System.out.print("자바를\n");
+        System.out.print("배웁니다.\n");
+
+//  Java 13부터 큰따옴표 3개로 감싸면 이스케이프하거나 라인피드 할 필요 없이 작성된 그대로 저장된다.
+        String str1 = ""+
+        "{\n"+
+        "\t\"id\":\"winter\",\n"+
+        "\t\"name\":\"눈송이\"\n"+
+        "}";
+
+        String str2 = """
+          {
+            "id":"winter",
+            "name":"눈송이"
+          }
+          """;
+        System.out.println(str1);
+        System.out.println(str2);
+
+        String str3 = """
+                나는 자바를 \
+                학습합니다.
+                나는 자바 고수가 될 겁니다.
+                """;
+        System.out.println(str3);
+    }
+}
