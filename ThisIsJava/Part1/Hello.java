2d1e623762b7ee3905be4450aa2c751514e78f94
choiyun9yu <sw22tapple@gmail.com>
2023-05-09 18:49:00 +0900

    main

---
+++ ThisIsJava/Part1/Hello.java
@@ -0,0 +1,19 @@
+package Part1;   // 패키지 선언
+
+public class Hello {    // 클래스 선언, 클래스명은 숫자로 시작할 수 없고, 공백 포함할 수 없다.
+
+    /**
+     * 도큐먼트 주석 : Javadoc 명령어로 API 도큐먼트를 생성하는데 사용
+     * @author : 최윤규
+     */
+
+    /*
+    범위주석
+    장제목: 1장 자바 시작하기
+    작성일: 2023. 05.09
+     */
+
+    public static void main(String[] args) {    // main() 메소드 선언, 프로그램 실행의 진입점
+        System.out.println("Hello, Java");  // Console 뷰에 출력하는 코드
+    }
+}
