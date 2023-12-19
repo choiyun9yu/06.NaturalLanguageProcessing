e97db6d67805611b3c82b4f02b60b53ba9b42115
choiyun9yu <sw22tapple@gmail.com>
2023-12-19 17:55:57 +0900

    update java-baisc

---
+++ Inflearn/java-basic/src/oop1/ex/RectangleOopMain.java
@@ -0,0 +1,15 @@
+package oop1.ex;
+
+public class RectangleOopMain {
+
+    public static void main(String[] args) {
+        Rectangle rectangle = new Rectangle();
+
+        rectangle.width = 5;
+        rectangle.height = 8;
+
+        rectangle.calculateArea();
+        rectangle.calcuatePerimeter();
+        rectangle.isSquare();
+    }
+}
