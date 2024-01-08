bdfb537f1ac1df0e5f523b376ffc9443301e2593
choiyun9yu <sw22tapple@gmail.com>
2024-01-08 10:51:16 +0900

    update: inheritance

---
+++ .example/Inflearn/java-basic/src/final08/ConstructInit.java
@@ -0,0 +1,10 @@
+package final08;
+
+public class ConstructInit {
+
+    final int value;
+
+    public ConstructInit(int value){
+        this.value = value;
+    }
+}
---
+++ .example/Inflearn/java-basic/src/final08/FieldInit.java
@@ -0,0 +1,12 @@
+package final08;
+
+public class FieldInit {
+
+    static final int CONST_VALUE = 10;
+    final int value = 10;
+
+//    public FieldInit(int value) {
+//        // 이미 초기값을 설정한 경우 생성자를 통해 값을 할당할 수 없다.
+//        // this.value = value;
+//    }
+}
