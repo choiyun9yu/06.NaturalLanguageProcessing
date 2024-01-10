86a8c0297c353fd638881c0b635d841617953675
choiyun9yu <sw22tapple@gmail.com>
2024-01-10 17:27:17 +0900

    update: inheritance

---
+++ .example/Inflearn/java-basic/src/extends09/access/ExtendsAccessMain.java
@@ -0,0 +1,11 @@
+package extends09.access;
+
+import extends09.access.child.Child;
+
+public class ExtendsAccessMain {
+
+    public static void main(String[] args) {
+        Child child = new Child();
+        child.call();
+    }
+}
