6bbb863002ecca5a1fff8747c3655fc8da190a21
choiyun9yu <sw22tapple@gmail.com>
2024-01-24 18:06:18 +0900

    update: interface

---
+++ example/Inflearn/java-basic/src/poly10/diamond/InterfaceA.java
@@ -0,0 +1,6 @@
+package poly10.diamond;
+
+public interface InterfaceA {
+    void methodA();
+    void methodCommon();
+}
---
+++ example/Inflearn/java-basic/src/poly10/diamond/InterfaceB.java
@@ -0,0 +1,6 @@
+package poly10.diamond;
+
+public interface InterfaceB {
+    void methodB();
+    void methodCommon();
+}
