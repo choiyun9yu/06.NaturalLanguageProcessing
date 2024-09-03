5a27989ca4e9f74c1187520c9e7746def096eab7
choiyun9yu <sw22tapple@gmail.com>
2024-09-03 11:28:40 +0900

    update array

---
+++ practice/Inflearn/java-mid2/src/collection/array/ArrayMain2.java
@@ -0,0 +1,24 @@
+package collection.array;
+
+import java.util.Arrays;
+
+public class ArrayMain2 {
+
+    public static void main(String[] args) {
+        int[] arr = new int[5];
+        arr[0] = 1;
+        arr[1] = 2;
+        System.out.println(Arrays.toString(arr));
+
+        // 배열의 첫번째이 위치에 추가
+        // 기존 배열의 데이터를 한 칸씩 뒤로 밀고 배열의 첫번째 위치에 추가
+        System.out.println("배열의 첫번째 위치에 3 추가 O(n)");
+        int newValue = 3;
+        addFirst(arr, newValue);
+    }
+
+    private static void addFirst(int[] arr, int newValue) {
+
+    }
+
+}
